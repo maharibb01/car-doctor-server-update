cba263efdce66b989f8a3b0e68215a15b9a3b59c
maharibb01 <maharibb01@gmail.com>
2024-04-06 16:20:33 +0600

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://cars-doctor-e6235.web.app",
+        "https://cars-doctor-e6235.firebaseapp.com"
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
-console.log(process.env.DB_PASS)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dbcluster.equ9ury.mongodb.net/?retryWrites=true&w=majority&appName=DBCluster`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -23,16 +31,57 @@ const client = new MongoClient(uri, {
     }
 });
 
+// custom middleware
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    console.log('token got in middleware', token);
+    if (!token) {
+        return res.status(401).send({ message: "unauthorized access" });
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decodedData) => {
+        if (error) {
+            return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.user = decodedData;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
-        const serviceCollection = client.db('carDoctor').collection('services');
-        const bookingCollection = client.db('carDoctor').collection('bookings');
+        const serviceCollection = client.db('cardoctor').collection('services');
+        const bookingCollection = client.db('cardoctor').collection('bookings');
 
+        // auth related api
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+            // console.log(token);
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "none"
+            }).send({ success: true })
+        })
+
+        app.post("/logout", async (req, res) => {
+            const user = req.body;
+            console.log("user on change", user);
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
+
+        // service related api
         app.get('/services', async (req, res) => {
-            const cursor = serviceCollection.find();
+            // const sort = req.query.sort;
+            // console.log(sort);
+            // const options = {
+            //     sort: { price: sort === "asc" ? 1 : -1 }
+            // }
+            const cursor = serviceCollection.find({ title: {$regex: req.query.search, $options: "i"} });
+            // const cursor = serviceCollection.find({ price: { $gt: 20, $lt: 250 } }, options);
             const result = await cursor.toArray();
             res.send(result);
         })
@@ -42,7 +91,6 @@ async function run() {
             const query = { _id: new ObjectId(id) }
 
             const options = {
-                // Include only the `title` and `imdb` fields in the returned document
                 projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
 
@@ -50,10 +98,13 @@ async function run() {
             res.send(result);
         })
 
-
         // bookings 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyToken, async (req, res) => {
             console.log(req.query.email);
+            console.log("token owner info", req.user);
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: "forbidden access" });
+            }
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
